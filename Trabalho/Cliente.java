f92f78d6249dc61b8d20ed781fcb05cabeaf0527
Vitoria <vick_santos_vick@hotmail.com>
2021-11-09 20:23:44 -0300

    Ajuste  2

--- Trabalho/Cliente.java
+++ Trabalho/Cliente.java
@@ -1,10 +1,10 @@
 public class Cliente {
     public static void main(String[] args) {
         
-        ProdutoFactory factory=  ProdutoFactory.getFactory(TipoProduto.Livro);
+        ProdutoFactory factory=  ProdutoFactory.getFactory(TipoProduto.Livro, "Qualquer", "0102");
 
         ProdutoInterface produto = factory.getProduto();
-
+        
         System.out.println(produto);
 
     }
